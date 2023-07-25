4bdf15762c9c23a1a80bed75dd939b3e75dc0fe1
Simon Davis <s.davis@hotmail.co.nz>
2023-07-25 16:30:33 +0100

    meditation model defined and migrated.

--- meditations/admin.py
+++ meditations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Meditation
 
 # Register your models here.
+
+admin.site.register(Meditation)
--- meditations/models.py
+++ meditations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Meditation(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    genre = models.CharField(max_length=10)
+    length = models.CharField(max_length=10)
+    thumbnail = models.URLField()
+    url = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='videos'
+
+    )
