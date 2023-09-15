0a1da279ff9362aefcf4c4056532c66eae254d77
Simon Davis <s.davis@hotmail.co.nz>
2023-09-15 12:21:51 +0100

    logout button added with handleLogout function added. No proper functionality, but can log button clicks to console.

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -6,10 +6,17 @@ import { isAuthenticated } from "../helpers/Auth"
 
 const Footer = (): JSX.Element => {
 
+
+  const handleLogout = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault()
+    console.log('Button clicked')
+
+  }
+
   return (
     <footer>
     { isAuthenticated() ? 
-      <button>Sign out</button>
+      <button onClick={handleLogout}>Sign out</button>
       :
       <p>hello</p>
     }
