e025a786e26dd0560596a6f4294bc8107a309b61
Simon Davis <s.davis@hotmail.co.nz>
2023-09-22 13:02:30 +0100

    can now input YouTube URL and save to state from within add video component

--- client/src/components/addVideo/AddVideo.tsx
+++ client/src/components/addVideo/AddVideo.tsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
+
+
 function AddVideo(): JSX.Element {
 
+  const [urlField, setURLField] = useState('')
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value)
+    setURLField(e.target.value)
+  }
+
+  const processLink = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log('input form state', urlField)
+  
+  }
+
   return (
-    <h1>ADD VIDEO</h1>
+    <main>
+      <h1>ADD VIDEO</h1>
+      <form onSubmit={processLink}>
+        <input type="text" placeholder="Insert video URL here" onChange={handleChange}></input>
+        <button type="submit">Process video</button>
+      </form>
+    </main>
+
   )
 }
 
