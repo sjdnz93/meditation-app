77b1eb8871e2eebc99947241048424e7f0d51bb5
Simon Davis <s.davis@hotmail.co.nz>
2024-06-04 17:22:24 +0300

    can now filter meditation videos by genre on home profile page

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -17,6 +17,8 @@ function Profile(): JSX.Element {
   const [userProfile, setUserProfile] = useState<UserProfile>()
   const [error, setError] = useState<string>('')
   const [updatedVideos, setUpdatedVideos] = useState<Video[]>([])
+  const [radioButton, setRadioButton] = useState<string>('All')
+  const [filteredVideos, setFilteredVideos] = useState<Video[]>([])
 
   useEffect(() => {
     const getProfile = async () => {
@@ -24,6 +26,7 @@ function Profile(): JSX.Element {
         const { data }: AxiosResponse<UserProfile> = await axios.get(`/api/profile/${sub}`)
         console.log('LOGGED USER DATA', data)
         setUserProfile(data)
+        setFilteredVideos(data.videos!)
       } catch (err: any) {
         console.log(err)
         if (axios.isAxiosError(err)) {
@@ -50,6 +53,21 @@ function Profile(): JSX.Element {
     }
   }
 
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRadioButton(event.target.value);
+    if (event.target.value === 'All') {
+      setFilteredVideos(userProfile!.videos!)
+    } else if (event.target.value === 'Guided') {
+      setFilteredVideos(userProfile!.videos!.filter(video => video.genre === 'Guided'))
+    } else if (event.target.value === 'Ambient') {
+      setFilteredVideos(userProfile!.videos!.filter(video => video.genre === 'Ambient'))
+    } else if (event.target.value === 'Body scan') {
+      setFilteredVideos(userProfile!.videos!.filter(video => video.genre === 'Body scan'))
+    } else if (event.target.value === 'Sleep') {
+      setFilteredVideos(userProfile!.videos!.filter(video => video.genre === 'Sleep'))
+    }
+  };
+
   return (
     <main>
       {userProfile && userProfile.email !== '' ?
@@ -60,7 +78,59 @@ function Profile(): JSX.Element {
             <Link to={'/add-video'}>
               Click to add meditation videos
             </Link>
-            {userProfile.videos!.length > 0 && userProfile.videos?.map(video => (
+            <div>
+              <label>
+                <input 
+                  type="radio" 
+                  value="All"
+                  checked={radioButton === 'All'}
+                  onChange={handleRadioChange}
+                  />
+                  All
+              </label>
+
+              <label>
+                <input 
+                  type="radio" 
+                  value="Guided"
+                  checked={radioButton === 'Guided'}
+                  onChange={handleRadioChange}
+                  />
+                  Guided
+              </label>
+
+              <label>
+                <input 
+                  type="radio" 
+                  value="Ambient"
+                  checked={radioButton === 'Ambient'}
+                  onChange={handleRadioChange}
+                  />
+                  Ambient
+              </label>
+
+              <label>
+                <input 
+                  type="radio" 
+                  value="Body scan"
+                  checked={radioButton === 'Body scan'}
+                  onChange={handleRadioChange}
+                  />
+                  Body scan
+              </label>
+
+              <label>
+                <input 
+                  type="radio" 
+                  value="Sleep"
+                  checked={radioButton === 'Sleep'}
+                  onChange={handleRadioChange}
+                  />
+                  Sleep
+              </label>
+            </div>
+
+            {filteredVideos!.length > 0 && filteredVideos?.map(video => (
               <div key={video.id}>
                 <p>{video.title}</p>
                 <p>{video.artist}</p>
